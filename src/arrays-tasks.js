25af66390fa1069ebffee62e873623dbe30b301b
Dmitry Malashev <dmalashev@gmail.com>
2024-12-08 02:35:51 +0300

    feat: solve the sumArrays task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,10 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  return arr1.length < arr2.length
+    ? arr1.map((item, i) => item + arr2[i]).concat(arr2.slice(arr1.length))
+    : arr2.map((item, i) => item + arr1[i]).concat(arr1.slice(arr2.length));
 }
 
 /**
