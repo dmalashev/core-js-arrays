53d3eb8834604811930e7254cadad0934f5dc31e
Dmitry Malashev <dmalashev@gmail.com>
2024-12-08 12:29:33 +0300

    feat: solve the getHexRGBValues task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -444,8 +444,12 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  const numToHex = (num) => {
+    return `#${num.toString(16).padStart(6, '0').toUpperCase()}`;
+  };
+
+  return arr.map((item) => numToHex(item));
 }
 
 /**
