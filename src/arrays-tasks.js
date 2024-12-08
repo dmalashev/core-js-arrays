fc79c2e66e1b324a416104a1df95608049150c0f
Dmitry Malashev <dmalashev@gmail.com>
2024-12-08 12:15:13 +0300

    feat: solve the getIndicesOfOddNumbers task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -426,8 +426,12 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce(
+    (indices, num, index) =>
+      num % 2 !== 0 ? indices.concat(index) : indices.concat(),
+    []
+  );
 }
 
 /**
