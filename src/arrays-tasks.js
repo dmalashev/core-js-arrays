26a2c0ec0001cda8e158719305ba81a6d97ccbfe
Dmitry Malashev <dmalashev@gmail.com>
2024-12-08 11:41:28 +0300

    feat: solve the createChunks task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -330,8 +330,12 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return arr
+    .filter((item, index) => index % chunkSize === 0)
+    .map((item, index) =>
+      arr.slice(index * chunkSize, chunkSize * (index + 1))
+    );
 }
 
 /**
